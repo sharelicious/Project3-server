84765cd8afcff67682d21fac71c9790bf935ed51
Samy Ali <samy.alisanchez91@gmail.com>
2022-06-03 08:13:15 +0200

    gitignore .env and password commented

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,9 @@
-//"/follow-friends"
-//"/friends"
-
 const { isAuthenticated } = require("../middlewares/jwt.middleware");
 const mongoose = require("mongoose");
 const router = require("express").Router();
 const User = require("../models/User.model");
 
-//all friends
+//get all friends
 router.get("/user/friends", isAuthenticated, (req, res) => {
   const allFriends = User.findById(req.payload._id); //username.find()
   Promise.all([allFriends])
@@ -42,19 +39,33 @@ router.get("/user/follow/:friendId", isAuthenticated, (req, res) => {
     });
 });
 
+//unfollowing friends
 router.get("/user/unfollow/:friendId", isAuthenticated, (req, res) => {
-    User.findById(req.payload._id)
-      .then((loggedInUser) => {
-        const friendIndex = loggedInUser.friends.indexOf(req.params.friendId)
-        loggedInUser.friends.splice(friendIndex, 1);
-        loggedInUser
-          .save() //db
-          .then((loggedInUser) => res.json(loggedInUser.friends));
-      })
-      .catch((err) => {
-        console.log("Unable to find friends", err);
-        res.redirect("/friends");
-      });
-  });
-  
+  User.findById(req.payload._id)
+    .then((loggedInUser) => {
+      const friendIndex = loggedInUser.friends.indexOf(req.params.friendId);
+      loggedInUser.friends.splice(friendIndex, 1);
+      loggedInUser
+        .save() //db
+        .then((loggedInUser) => res.json(loggedInUser.friends));
+    })
+    .catch((err) => {
+      console.log("Unable to find friends", err);
+      res.redirect("/friends");
+    });
+});
+
+//check if the users are following each other
+router.get("/user/checkfollowing/:friendId", isAuthenticated, (req, res) => {
+  User.findById(req.payload._id)
+    .then((loggedInUser) => {
+      const isFollowing = loggedInUser.friends.includes(req.params.friendId);
+      res.json(isFollowing);
+    })
+    .catch((err) => {
+      console.log("Unable to find friends", err);
+      res.redirect("/friends");
+    });
+});
+
 module.exports = router;
