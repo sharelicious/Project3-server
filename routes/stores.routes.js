1026ab73e7746a4e2fb413055131ed0b96fe074b
Pablo Dellacassa <pdellacassa@gmail.com>
2022-06-03 13:07:52 +0200

    upload last changes in store routes

--- routes/stores.routes.js
+++ routes/stores.routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const Store = require("../models/Store.model");
 const Product = require("../models/Product.model");
-/* const Comment = require("../models/Comments.model"); */
+/* const Comment = require("../models/Comments.model");*/
 const User = require("../models/User.model");
 const mongoose = require("mongoose");
 const { isAuthenticated } = require("../middlewares/jwt.middleware");
@@ -10,68 +10,56 @@ router.get("/cuisine/:type", isAuthenticated, (res, req) => {
 Store.find({ cuisineType: req.params.type })
     .populate("products")
     .then((stores) => {
-      res.json(stores)
+      res.status(201).json(stores)
     })
     .catch((err) => {
-      res.json(err)
+      res.status(500).json(err)
     })
 });
 
-router.get("/friends-stores", isAuthenticated, (req, res) => {
-  User.find({ friends: req.user._id })
-    .populate("favoriteStores")
+router.get("/friends-stores",isAuthenticated, (req, res) => {
+  User.findById(req.payload._id) // req.payload._id is the user's id
+    .populate({
+      path : 'friends',
+      populate : {
+        path : 'favoriteStores'
+      }
+    })
+    .then((user) => {
+      res.status(201).json(user.friends)
+    })
+    .catch((err) => {
+      res.status(500).json(err)
+    })
+});
+
+router.get("/cuisine-results", isAuthenticated, (req, res) => {
+  Store.find()
+    .populate("products")
+    .populate("comments")
     .then((stores) => {
-      res.json(stores)
+      res.status(201).json(stores)
     })
     .catch((err) => {
-      res.json(err)
+      res.status(500).json(err)
     })
-});
-
-
-router.get("/getAllCuisines", isAuthenticated, (req, res, next) => {
-  const allCuisines = Product.find();
-
-  Promise.all([allCuisines])
-    .then(([cuisines]) => {
-      res.json(filteredStores(cuisines));
-    })
-    .catch((err) => res.status(500).json(err));
-});
-
-router.get("/getAllFavoriteStores", isAuthenticated, (req, res, next) => {
-  const allFavoritesStores = User.find();
-  
-
-  Promise.all([allFavoritesStores])
-    .then(([favoritesStores]) => {
-      res.json(filteredStores(favoritesStores));
-    })
-    .catch((err) => res.status(500).json(err));
-});
-
-
-router.get("/filter/:cuisines", isAuthenticated, (req, res) => {
-  const { cuisineType } = req.params;
-
-  Store.find({ cuisineType })
-    .then((cuisineGroups) => {
-      res.status(201).json(cuisineGroups);
-    })
-    .catch((err) => res.status(500).json(err));
-});
-
-router.get('/filter/:stores', (req, res, next) => {
-
-  const { favoritesStores } = req.params
-
-  User
-      .find({ favoritesStores })
-      .then(stores => res.json(stores))
-      .catch(err => res.status(500).json(err))
 })
 
 
+router.get("/store-details/:storeId", isAuthenticated, (req, res) => {
+  Store.findById(req.params.storeId)
+    .populate("products")
+    .populate("comments")
+    .then((store) => {
+      res.status(201).json(store)
+    })
+    .catch((err) => {
+      res.status(500).json(err)
+    })
+});
+
+
+  
 
 
 
@@ -79,28 +67,3 @@ router.get('/filter/:stores', (req, res, next) => {
 
 module.exports = router;
 
-/* router.get("/getOneCoaster/:coaster_id", (req, res) => {
-
-  const { coaster_id } = req.params
-
-  Coaster
-    .findById(coaster_id)
-    .then(response => res.json(response))
-    .catch(err => res.status(500).json(err))
-})
-
-
-router.post("/saveCoaster", isAuthenticated, (req, res) => {
-
-  const { title, description, imageUrl, inversions, length } = req.body
-
-  const owner = req.payload._id
-
-  console.log('El payload del objeto request nos da en las rutas protegidas el ID logueado:', owner)
-
-  Coaster
-    .create({ title, description, imageUrl, inversions, length, owner })
-    .then(response => res.json(response))
-    .catch(err => res.status(500).json(err))
-})
-  */
