eed340ecb5b3c481d852c2997f8aec559871c56a
Samy Ali <samy.alisanchez91@gmail.com>
2022-06-03 14:06:23 +0200

    index.routes updated

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,26 +4,9 @@ router.get("/", (req, res, next) => {
   res.json("All good in here");
 })
 
-<<<<<<< HEAD
-
- router.use("/auth", require('./auth.routes'))
- router.use("/user", require('./user.routes'))
- router.use("/store", require('./stores.routes'))
-
-
- /* 
-router.use("/upload", require('./upload.routes'))  
-router.use("/store", require('./stores.routes'))
-*/
-
-
-
-
-=======
 router.use("/auth", require('./auth.routes'))
 router.use("/store", require('./stores.routes'))
 router.use("/user", require('./user.routes'))
 router.use("/upload", require('./upload.routes'))
->>>>>>> 53d801ac6172b979a366e3794519e68ab165486d
 
 module.exports = router
