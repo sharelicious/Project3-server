2ed6dcd5d0115d6a72a7430e4d3a7ab7ab4f97f3
Luisa Ojeda <luisaojeda@gmail.com>
2022-06-06 17:38:36 +0200

    Likes routes

---
+++ routes/likes.routes.js
@@ -0,0 +1,25 @@
+const { isAuthenticated } = require("../middlewares/jwt.middleware");
+const mongoose = require("mongoose");
+const router = require("express").Router();
+const User = require("../models/User.model");
+const Store = require("../models/Store.model");
+
+//actions on like button
+router
+.post("/:storeId/like", (req, res) => {
+    const { id } = req.params;
+    Store.findByIdAndUpdate(id, {$push: { storeLikes: req.payload._id }})
+    .then((updatedStore) => {
+        User.findByIdAndUpdate(req.payload._id, {$push: { favoriteStores: updatedStore._id}})})
+        .then((__) => {
+            res.status(201).json('liked correctly!')
+        })
+    .catch((err) => {
+        res.status(500).json(err);
+    })
+})
+
+// See all likes of the user 
+router
+.get 
+
