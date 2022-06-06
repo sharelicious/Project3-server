8b5301ca81bfe25431eebc3dc36957db576970cf
Pablo Dellacassa <pdellacassa@gmail.com>
2022-06-06 16:35:56 +0200

    changes in routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 
 router.get("/", (req, res, next) => {
+  console.log(req);
   res.json("All good in here");
 })
 
@@ -8,7 +9,7 @@ router.use("/auth", require('./auth.routes'))
 router.use("/store", require('./stores.routes'))
 router.use("/user", require('./user.routes'))
 router.use("/upload", require('./upload.routes'))
-router.use("/", require('./profile.routes'))
+router.use("/profile", require('./profile.routes'))
 
 
 module.exports = router
---
+++ shared/cuisine-types.constants.js
@@ -0,0 +1,22 @@
+const cuisineTypes = [
+  "Pizza",
+  "Sushi",
+  "Thai",
+  "Chinese",
+  "Ramen",
+  "Soup",
+  "Italian",
+  "Vegan",
+  "Healthy",
+  "Indian",
+  "Burgers",
+  "Breakfast",
+  "Salad",
+  "Mexican",
+  "Vegetarian",
+  "Sandwiches",
+  "Mediterranean",
+  "Korean",
+];
+
+module.exports = {cuisineTypes};
