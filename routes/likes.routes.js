80c04f37ce9f7d0b95c4b05636a52686bccd1fee
Samy Ali <samy.alisanchez91@gmail.com>
2022-06-09 14:56:20 +0200

    likes done

--- routes/likes.routes.js
+++ routes/likes.routes.js
@@ -5,66 +5,69 @@ const User = require("../models/User.model");
 const Store = require("../models/Store.model");
 
 //actions on like button
-router
-.post("/:storeId", isAuthenticated, (req, res) => {
-    const id  = req.params.storeId;
-    User.findByIdAndUpdate(req.payload._id, {$push: { favoriteStores: id}})
-    .then((user) => {
-        Store.findByIdAndUpdate(id, {$push: { storeLikes: req.payload._id }}, {new: true})
-        .then((store) => {
-            res.status(201).json(store.storeLikes)
+router.post("/:storeId", isAuthenticated, (req, res) => {
+  const id = req.params.storeId;
+  User.findById(req.payload._id).then((user) => {
+    if (!user.favoriteStores.includes(id)) {
+      User.findByIdAndUpdate(req.payload._id, { $push: { favoriteStores: id } })
+        .then((user) => {
+          Store.findByIdAndUpdate(
+            id,
+            { $push: { storeLikes: req.payload._id } },
+            { new: true }
+          ).then((store) => {
+            res.status(201).json(store.storeLikes);
+          });
         })
-    })
-    .catch((err) => {
-        res.status(500).json(err);
-    })
-})
+        .catch((err) => {
+          res.status(500).json(err);
+        });
+    }
+  });
+});
 
-// See all likes of the user 
-router
-.get ("/:storeId/like/:userId", (req, res) => {
-    User.findById(req.payload._id)
+// See all likes of the user
+router.get("/:storeId/like/:userId", (req, res) => {
+  User.findById(req.payload._id)
     .populate("favoriteStores")
     .then((user) => {
-        const reverseLikes = user.favoriteStores.reverse();
-        res.status(200).json(reverseLikes)
+      const reverseLikes = user.favoriteStores.reverse();
+      res.status(200).json(reverseLikes);
     })
-    .catch((err) => console.log(err))
-})
+    .catch((err) => console.log(err));
+});
 
 //See all friend's favourites restaurants
 router.get("/friendsStores", isAuthenticated, (req, res) => {
-    
-    User.findById(req.payload._id)
-      .populate({
-        path: "friends",
-        populate: {
-          path: "favoriteStores",
+  User.findById(req.payload._id)
+    .populate({
+      path: "friends",
+      populate: {
+        path: "favoriteStores",
+      },
+    })
+    .then((user) => {
+      const friendsLikesArr = [];
+
+      user.friends.forEach((friend) => {
+        if (friend.favoriteStores.length > 0) {
+          const randomIndex = Math.floor(
+            Math.random() * friend.favoriteStores.length
+          );
+          const newItem = {
+            friend: friend,
+            store: friend.favoriteStores[randomIndex],
+          };
+
+          friendsLikesArr.push(newItem);
         }
-      })
-      .then((user) => {
-        const friendsLikesArr = [];
-
-        user.friends.forEach((friend) => {
-
-            if(friend.favoriteStores.length > 0) {
-                const randomIndex = Math.floor(Math.random() * friend.favoriteStores.length);
-                const newItem = {
-                    friend: friend, 
-                    store: friend.favoriteStores[randomIndex]
-                }
-
-                friendsLikesArr.push(newItem);
-            }
-        })
-
-        res.status(201).json(friendsLikesArr);
-      })
-      .catch((err) => {
-        res.status(500).json(err);
       });
-  });
 
-module.exports = router
+      res.status(201).json(friendsLikesArr);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
 
-
+module.exports = router;
